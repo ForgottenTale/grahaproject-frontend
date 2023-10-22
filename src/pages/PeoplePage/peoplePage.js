1a30b4a0bd7ff97a1ab329fd915f36dc8e134f24
Abhijith Kannan <abhijithkannan452@gmail.com>
2023-10-22 17:52:05 +0530

    update

--- src/pages/PeoplePage/peoplePage.js
+++ src/pages/PeoplePage/peoplePage.js
@@ -88,7 +88,7 @@ export default function PeoplePage() {
                             <div className="keyProjectsTitle">Key Projects</div>
                             <div className="keyProjectsList">
                                 {
-                                    person?.attributes?.keyProj && person?.attributes?.keyProj?.map((proj) => (
+                                    person?.attributes?.keyProjects && person?.attributes?.keyProjects.split(",").map((proj) => (
                                         <div className="keyItem" ><svg className="itemDash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 288h-352c-17.69 0-32-14.32-32-32.01s14.31-31.99 32-31.99h352c17.69 0 32 14.3 32 31.99S417.7 288 400 288z" /></svg>
                                             {proj}
                                         </div>
